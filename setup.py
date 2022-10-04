01c4081af05b00175eba52f74303f92d2f58d374
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2022-10-04 17:27:04 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Software SUN PHY modulator'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name="sun_phy",
+    version=VERSION,
+    author="Sebastien Deriaz",
+    author_email="sebastien.deriaz1@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', '802.15.4g', 'mr-fsk', 'mr-o-qpsk', 'mr-ofdm', 'ofdm', 'modulation', 'modulator', 'sdr', 'iq'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
