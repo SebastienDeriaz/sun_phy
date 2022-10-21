35e3af0b40010390e53738847dbc076207b46b2e
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2022-10-21 21:11:25 +0200

    add unsupported error management

--- sun_phy/tests/test_mr_ofdm.py
+++ sun_phy/tests/test_mr_ofdm.py
@@ -9,15 +9,29 @@ def test_parameters():
 
     This function does not test whether or not the output signal is valid
     """
-    def is_unsuported(fb, rm, sm):
-        output = False
-        return output
+    def is_unsuported(MCS, OFDM_Option, phyOFDMInterleaving, scrambler):
+        return (OFDM_Option == 1 and MCS not in [0, 1, 2, 3] ) or \
+            (OFDM_Option == 2 and MCS not in [0, 1, 2, 3, 4, 5]) or \
+            (OFDM_Option == 3 and MCS not in [1, 2, 3, 4, 5, 6]) or \
+            (OFDM_Option == 4 and MCS not in [2, 3, 4, 5, 6])
+            
 
 
-    for MCS in range(8):
+    for MCS in range(7):
         for OFDM_Option in range(1, 5):
-            for phyOFDMInterleaving in range(2):
-                for scrambler in range(4):
+            for scrambler in range(4):
+                for phyOFDMInterleaving in range(2):
                     # Instanciate modulator
-                    modulator = Mr_ofdm_modulator(MCS=MCS, OFDM_Option=OFDM_Option, phyOFDMInterleaving=phyOFDMInterleaving, scrambler=scrambler)
-                    modulator.messageToIQ(b'test_message')
+                    if is_unsuported(MCS, OFDM_Option, phyOFDMInterleaving, scrambler):
+                        # Expect an UnsupportedError
+                        try:
+                            modulator = Mr_ofdm_modulator(MCS=MCS, OFDM_Option=OFDM_Option, phyOFDMInterleaving=phyOFDMInterleaving, scrambler=scrambler)
+                        except UnsupportedError as e:
+                            # got it !
+                            continue
+                        else:
+                            raise ValueError("Unsupported parameters combination wasn't raised")
+                    else:
+                        modulator = Mr_ofdm_modulator(MCS=MCS, OFDM_Option=OFDM_Option, phyOFDMInterleaving=phyOFDMInterleaving, scrambler=scrambler)
+                    
+                    modulator.message_to_IQ(b'test_message', binary=False)
